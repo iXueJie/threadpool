863c28dfe107cb520047385fa3610f9108d77532
iXueJie <1419251768@qq.com>
2021-12-14 19:38:57 +0800

    Modified ./build

---
+++ build/CMakeFiles/taskqueue.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taskqueue.dir/taskqueue.c.obj"
+  "CMakeFiles/taskqueue.dir/taskqueue.c.obj.d"
+  "libtaskqueue.a"
+  "libtaskqueue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/taskqueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/taskqueue.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for taskqueue.
+# This may be replaced when dependencies are built.
